d6a2008cfc6f8601c482223a2c23726911668c92
Steve Ungerer <steve@crimsonhexagon.com>
2021-03-22 15:30:13 -0400

    Correct JFastText.getLabels()
    Code was incorrectly invoking `getWords` for the call to `getLabels()`

--- src/main/java/com/github/jfasttext/JFastText.java
+++ src/main/java/com/github/jfasttext/JFastText.java
@@ -188,7 +188,7 @@ public class JFastText {
     }
 
     public List<String> getLabels() {
-        return stringVec2Strings(fta.getWords());
+        return stringVec2Strings(fta.getLabels());
     }
 
     public double getLr() {
